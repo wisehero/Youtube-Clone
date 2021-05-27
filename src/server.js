c7ca556e43642eb47debdf81f94d38b3070e9278
김지웅 <godjw96@gmail.com>
2021-05-27 16:05:23 +0900

    3.1 Making our Router

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import express from "express"; // 서버 구축의 시작
-import morgan from "mogran";
+import morgan from "morgan";
 
 const PORT = 4000;
 
@@ -9,9 +9,23 @@ app.use(logger);
 
 // Global Router
 const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
 // Local Router
 const userRoudter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRoudter.get("/edit", handleEditUser);
+
 const videoRouter = express.Router();
+
+videoRouter.get("/watch", handleWatchVideo);
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
 
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
