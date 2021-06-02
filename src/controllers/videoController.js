1f150fc82fb2a65f6625331901462f8edf3b3b91
김지웅 <godjw96@gmail.com>
2021-06-03 02:03:05 +0900

    Edit Video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -8,15 +8,32 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
   return res.render("watch", { pageTitle: video.title, video });
 };
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Editing` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  return res.render("edit", { pageTitle: `Edit ${video.title}`, video });
 };
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  video.title = title;
+  video.description = description;
+  video.hashtags = hashtags
+    .split(",")
+    .map((word) => (word.startWith("#") ? word : `#${word} `));
+  await video.save();
   return res.redirect(`/videos/${id}`);
 };
 
@@ -31,7 +48,9 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags.split(",").map((word) => `#${word} `),
+      hashtags: hashtags
+        .split(",")
+        .map((word) => (word.startWith("#") ? word : `#${word} `)),
     });
     return res.redirect("/");
   } catch (error) {
--- src/models/Video.js
+++ src/models/Video.js
@@ -4,7 +4,7 @@ const videoSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   createdAt: { type: Date, required: true, default: Date.now },
-  hastags: [{ type: String }],
+  hashtags: [{ type: String }],
   meta: {
     views: { type: Number, default: 0, required: true },
     rating: { type: Number, default: 0, required: true },
