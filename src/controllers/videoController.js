b8d3e9f498179c1eaa114e61d013acd4f574ce95
김지웅 <godjw96@gmail.com>
2021-06-02 13:10:06 +0900

    Exception and Vailidation

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -26,16 +26,23 @@ export const getUpload = (req, res) => {
 export const postUpload = async (req, res) => {
   // here we will add a video to the videos array.
   const { title, description, hashtags } = req.body;
-  await Video.create({
-    title,
-    description,
-    createdAt: Date.now(),
-    hashtags: hashtags.split(",").map((word) => `#${word} `),
-    meta: {
-      views: 0,
-      rating: 0,
-    },
-  });
-
-  return res.redirect("/");
+  try {
+    await Video.create({
+      title,
+      description,
+      createdAt: Date.now(),
+      hashtags: hashtags.split(",").map((word) => `#${word} `),
+      meta: {
+        views: 0,
+        rating: 0,
+      },
+    });
+    return res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
--- src/init.js
+++ src/init.js
@@ -2,7 +2,7 @@ import "./db";
 import "./models/Video";
 import app from "./server";
 
-const PORT = 3000;
+const PORT = 4000;
 
 // this is callback
 const handleListening = () =>
