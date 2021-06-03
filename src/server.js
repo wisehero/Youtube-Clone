6a6aba1bacfe297c82c9e0a7cc291a07874a215c
김지웅 <godjw96@gmail.com>
2021-06-03 23:43:43 +0900

    env

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from "express"; // 서버 구축 시작
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express(); // express 애플리케이션 생성
 const logger = morgan("dev");
@@ -12,13 +14,18 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
 app.use(
   session({
-    secret: "Hello!",
-    resave: true,
-    saveUninitialized: true,
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    // 몽고 디비랑 연결
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
+
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
