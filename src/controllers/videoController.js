23dc875b1478e0b7def6bd54fc2a012bb170855c
김지웅 <godjw96@gmail.com>
2021-06-03 15:52:59 +0900

    search Part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -66,7 +66,14 @@ export const deleteVideo = async (req, res) => {
   return res.redirect("/");
 };
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    //search
+    videos = await Video.find({
+      title: { $regex: new RegExp(`${keyword}$`, "i") },
+    });
+  }
   return res.render("search"), { pageTitle: title };
 };
