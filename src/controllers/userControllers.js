f85efc7d727c7a7d316f368619fd325145dd23ed
김지웅 <godjw96@gmail.com>
2021-06-04 13:15:19 +0900

    edit profile and protector and public middlewares

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -139,6 +139,14 @@ export const logout = (req, res) => {
   req.session.destroy();
   return res.redirect("/");
 };
+
+export const getEdit = (req, res) => {
+  return res.render("edit-profile", { pageTitle: "Edit Profile" });
+};
+
+export const postEdit = (req, res) => {
+  return res.return("edit-profile");
+};
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const see = (req, res) => res.send("See User");
