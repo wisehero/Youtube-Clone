f226d8584f07098a8f5a2ae45c832bbfbd5e92e2
김지웅 <godjw96@gmail.com>
2021-06-04 16:19:11 +0900

    video Upload

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -42,11 +42,13 @@ export const getUpload = (req, res) => {
 
 export const postUpload = async (req, res) => {
   // here we will add a video to the videos array.
+  const { path: fileUrl } = req.file;
   const { title, description, hashtags } = req.body;
   try {
     await Video.create({
       title,
       description,
+      fileUrl,
       hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
