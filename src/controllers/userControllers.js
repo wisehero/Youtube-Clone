b19d48e1f249360399bd42b712f1fc5ce35d63f0
김지웅 <godjw96@gmail.com>
2021-06-04 14:57:07 +0900

    File Uploads part Two

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -150,7 +150,9 @@ export const postEdit = async (req, res) => {
       user: { _id },
     },
     body: { name, email, username, location },
+    file,
   } = req;
+  console.log(file);
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
@@ -161,8 +163,8 @@ export const postEdit = async (req, res) => {
     },
     { new: true }
   );
-
-  return res.return("edit-profile");
+  req.session.user = updatedUser;
+  return res.redirect("/users/edit");
 };
 
 export const getChangePassword = (req, res) => {
--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,5 @@
 import { render } from "pug";
+import multer from "multer";
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
@@ -22,3 +23,7 @@ export const publicOnlyMiddleware = (req, res, next) => {
     return res.redirect("/");
   }
 };
+
+export const uploadFiles = multer({
+  dest: "uploads/",
+});
