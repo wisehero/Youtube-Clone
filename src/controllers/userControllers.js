3f35348127ecd7ea504c63c6366e2102b673ca15
김지웅 <godjw96@gmail.com>
2021-06-04 10:25:10 +0900

    log Out

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -135,7 +135,10 @@ export const finishGithubLogin = async (req, res) => {
     return res.redirect("/login");
   }
 };
-export const logout = (req, res) => res.send("Log out");
+export const logout = (req, res) => {
+  req.session.destroy();
+  return res.redirect("/");
+};
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const see = (req, res) => res.send("See User");
