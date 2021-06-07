4c234ed5b793270960962831ba1cce7cd9f28d5c
김지웅 <godjw96@gmail.com>
2021-06-07 14:33:46 +0900

    Webpack Configuration part Two

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,24 @@
+const path = require("path");
+
 module.exports = {
   entry: "./src/client/js/main.js", // 우리가 변경하고자 하는 파일
+  mode: "development",
   // 변경하고자 하는 파일을 저장하는 곳
   output: {
     filename: "main.js",
-    path: "./assets/js",
+    path: path.resolve(__dirname, "assets", "js"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
   },
 };
