647850ae80a2b42ffd0560f2f738b844319c7cae
김지웅 <godjw96@gmail.com>
2021-06-08 15:51:13 +0900

    Styles

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,9 @@ import Video from "../models/Video";
 import User from "../models/User";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({}).sort({ createdAt: "desc" });
+  const videos = await Video.find({})
+    .sort({ createdAt: "desc" })
+    .populate("owner");
   return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -101,7 +103,7 @@ export const search = async (req, res) => {
     //search
     videos = await Video.find({
       title: { $regex: new RegExp(`${keyword}$`, "i") },
-    });
+    }).populate("owner");
   }
   return res.render("search"), { pageTitle: title };
 };
