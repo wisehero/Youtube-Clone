3d0950bebf2ecf42bbbb63375c48feab8618aceb
김지웅 <godjw96@gmail.com>
2021-07-20 01:25:23 +0900

    Update videoController.js

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -112,7 +112,7 @@ export const registerView = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   if (!video) {
-    return res.sendStatus(404)(404);
+    return res.sendStatus(404);
   }
   video.meta.views = video.meta.views + 1;
   await video.save();
