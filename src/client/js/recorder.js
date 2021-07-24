130ee6cac8a92875093b5cb5c913abfd1c048fe3
김지웅 <godjw96@gmail.com>
2021-07-24 15:28:16 +0900

    Download Video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -5,11 +5,17 @@ const video = document.getElementById("preview");
 
 let stream;
 let recorder;
-
-const handleDownload = () => {};
+let videoFile;
+const handleDownload = () => {
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "MyRecording.webm"; // a 태그의 download는 URL을 저장한다.
+  document.body.appendChild(a);
+  a.click(); // 사용자가 링크를 클릭한 것 처럼 작동하게 한다.
+};
 
 const handleStop = () => {
-  startBtn.innerText = "Start Recording";
+  startBtn.innerText = "Download Recording";
   startBtn.removeEventListener("click", handleStop); // Stop Recoding을 누르면 handleStop 이벤트가 사라지고
   startBtn.addEventListener("click", handleStart); //handleStart가 실행된다.
   startBtn.addEventListener("click", handleDownload);
@@ -20,11 +26,11 @@ const handleStart = async () => {
   startBtn.innerText = "Stop Recording";
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
-  const recorder = new MediaRecorder(stream);
+  recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
   recorder.ondataavailable = (e) => {
     recorder = new MediaRecorder(stream);
     recorder.ondataavailable = (event) => {
-      const videoFile = URL.createObjectURL(event.data);
+      videoFile = URL.createObjectURL(event.data);
       video.srcObject = null;
       video.src = videoFile;
       video.loop = true;
