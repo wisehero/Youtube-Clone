87943661db4d1a3d83ab2e1b7b5ef1fbb46968e7
김지웅 <godjw96@gmail.com>
2021-07-24 14:21:44 +0900

    recorder setup

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   entry: {
     main: "./src/client/js/main.js",
     videoPlayer: "./src/client/js/videoPlayer.js",
+    recorder: "./src/client/js/recorder.js",
   }, // 우리가 변경하고자 하는 파일
   mode: "development",
   watch: true,
