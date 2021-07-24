252dcbc388f32f619281a0f3f87adc72e50da5eb
김지웅 <godjw96@gmail.com>
2021-07-24 14:30:25 +0900

    Recording Video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,10 +1,43 @@
+import { async } from "regenerator-runtime";
+
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
+
+let stream;
+
+const handleStop = () => {
+  startBtn.innerText = "Start Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+};
 
 const handleStart = async () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+  const recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (e) => {
+    console.log("recording done");
+    console.log(e);
+    console.log(e.data);
+  };
+  console.log(recorder);
+  recorder.start();
+  console.log(recorder);
+  setTimeout(() => {
+    recorder.stop();
+  }, 10000);
+};
+
+const init = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
+    audio: false,
     video: true,
   });
+  video.srcObject = stream;
+  video.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);
